237490ddb794fda8a8b8cd56e10ea0f4b506cbec
benoit delville <benoit.delville@coretex.com>
2018-03-17 11:39:04 +1300

    async await & model

--- server-api/api/models/incident.js
+++ server-api/api/models/incident.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose');
 
 const incidentSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    location: String
+  _id: { type: String, required: true },
+  name: String,
+  whenOccurred: Date,
+  cyclist: {
+    age: Number,
+  },
+  type: String,
+  weather: String,
+  location: {
+    latitude: Number,
+    longitude: Number,
+    address: String,
+    routeId: String, // OpenStreetMap route id ?
+  },
+  accident: {
+    description: String,
+    url: String,
+  },
 });
 
 module.exports = mongoose.model('Incident', incidentSchema);
--- server-api/api/routes/incidents.js
+++ server-api/api/routes/incidents.js
@@ -1,16 +1,81 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const Incident = require("../models/incident");
+const express = require('express');
+const mongoose = require('mongoose');
+const uuidv1 = require('uuid/v1');
+
+const Incident = require('../models/incident');
 
 const router = express.Router();
 
-router.get("/", (req,res,next) => {
-    Incident.find()
+router.get('/', async (req, res, next) => {
+  try {
+    const incidents = await Incident.find().exec();
+    res.status(200).json(incidents);
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    });
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const incident = new Incident(
+      Object.assign({
+          _id: uuidv1(),
+        },
+        req.body,
+      ));
+
+    const result = await incident.save();
+    console.log(result);
+    res.status(201).json({
+      message: 'Handling POST requests to /incident',
+      createdIncident: result
+    });
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    });
+  }
+});
+
+router.get('/:incidentId', (req, res, next) => {
+  const id = req.params.incidentId;
+  Incident.findById(id)
     .exec()
-    .then(docs => {
-      console.log(docs);
-      res.status(200).json(docs);
-     })
+    .then(doc => {
+      console.log('From database', doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'No valid entry found for provided ID' });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+
+
+router.patch('/:incidentId', (req, res, next) => {
+  const id = req.params.incidentId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Incident.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json(result);
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json({
@@ -18,69 +83,6 @@ router.get("/", (req,res,next) => {
       });
     });
 });
-
-router.post("/", (req,res,next) => {
-    const incident = new Incident({
-        _id: new mongoose.Types.ObjectId(),
-        name: req.body.name,
-        location: req.body.location
-      });
-      incident
-        .save()
-        .then(result => {
-          console.log(result);
-          res.status(201).json({
-            message: "Handling POST requests to /incident",
-            createdIncident: result
-          });
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(500).json({
-            error: err
-          });
-        });
-});
-
-router.get("/:incidentId", (req, res, next) => {
-    const id = req.params.incidentId;
-    Incident.findById(id)
-      .exec()
-      .then(doc => {
-        console.log("From database", doc);
-        if (doc) {
-          res.status(200).json(doc);
-        } else {
-          res
-            .status(404)
-            .json({ message: "No valid entry found for provided ID" });
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({ error: err });
-      });
-  });
-  
-  router.patch("/:incidentId", (req, res, next) => {
-    const id = req.params.incidentId;
-    const updateOps = {};
-    for (const ops of req.body) {
-      updateOps[ops.propName] = ops.value;
-    }
-    Incident.update({ _id: id }, { $set: updateOps })
-      .exec()
-      .then(result => {
-        console.log(result);
-        res.status(200).json(result);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
-        });
-      });
-  });
 
 module.exports = router;
 
